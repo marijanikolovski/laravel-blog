1aac60c3a4a08243dd09ed35a821a24bc3996e4e
marijanikolovski <m.kuzeljevic@gmail.com>
2022-09-13 23:52:15 +0200

    Deleting a post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -112,11 +112,14 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  string  $slug
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $post = Post::where('slug', $slug);
+        $post->delete();
+
+        return redirect('/blog');
     }
 }
